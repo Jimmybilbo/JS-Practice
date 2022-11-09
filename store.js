603ae06148491790699b9f54cf3a30a21428ea6e
SamDuncan <jacobsteck@outlook.com>
2022-11-09 10:58:43 -0500

    First commit; playing with Maps data structure

---
+++ store.js
@@ -0,0 +1,25 @@
+// Store Selection Map
+var store = new Map ([
+    ["album1", 12.99],
+    ["album2", 14.99],
+    ["album3", 9.99],
+    ["album4", 19.99],
+    ["tshirt", 19.99],
+    ["coffCup", 6.99]
+])
+
+
+// Testing 
+console.log(store);
+console.log(store.get("album1"))
+
+function addCart(index) {
+    if (index === 0) { console.log(store.get("album1")) }
+    else if (index === 1) { console.log(store.get("album2")) }
+    else if (index === 2) { console.log(store.get("album2")) }
+    else if (index === 3) { console.log(store.get("album2")) }
+    else if (index === 4) { console.log(store.get("tshirt")) }
+    else if (index === 5) { console.log(store.get("coffCup")) }
+
+    console.log("Yousa Bitch")
+}
